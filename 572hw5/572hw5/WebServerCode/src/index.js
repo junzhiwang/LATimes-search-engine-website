3adc1aceecc5dd4e970322d0e7a4cac2b3b20a7c
Junzhi Wang <junzhiwa@Junzhis-MacBook-Pro.local>
2017-06-10 03:38:17 -0700

    modified

--- 572hw5/572hw5/WebServerCode/src/index.js
+++
@@ -1,6 +0,0 @@
-import SearchBar from './SearchBar';
-import ReactDOM from 'react-dom';
-ReactDOM.render(
-  <SearchBar />,
-  document.getElementById('search')
-);
--- 572hw5/Archive/WebServerCode/dist/router/index.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-var indexRouter = express.Router();
-import path from 'path';
-
-indexRouter.get("/", function (req, res) {
-    res.sendFile("index.html", { root: path.join(__dirname, '..')});
-});
-
-module.exports = indexRouter;
